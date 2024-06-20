a1f99418b249cba7b707fafd2c5339c41bcf51c2
Rudolf-Sijbrandij <rudolf.sijbrandij@student.hu.nl>
2024-06-20 11:41:52 +0200

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,8 +1,16 @@
 from transition import Transition
+from collections import deque 
+import random
 
 
 class Memory:
-    pass
+    def __init__(self):
+        self.transition_deque = deque() 
 
-    def get_memory(state, n_entries):
-        pass
+    def store(self, transitie: Transition):
+        self.transition_deque.append(transitie)
+        return
+
+    def sample(self, batch_size):
+        batch = random.sample(list(self.transition_deque), batch_size)
+        return batch
